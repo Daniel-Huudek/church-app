06faad307bc798af17460ee36a6d9043ae267a54
Daniel-Huudek <139517145+Daniel-Huudek@users.noreply.github.com>
2026-05-21 23:50:03 -0300

    Reapply "Merge pull request #4 from Daniel-Huudek/develop-test"
    This reverts commit 39afcfe8e8ff793dcf8a2c11f6b84892f914adc3.

--- apps/mobile/src/components/animations/index.ts
+++
@@ -1,8 +0,0 @@
-export { FadeIn } from './FadeIn';
-export type { FadeInProps } from './FadeIn';
-export { ScaleIn } from './ScaleIn';
-export type { ScaleInProps } from './ScaleIn';
-export { SlideUp } from './SlideUp';
-export type { SlideUpProps } from './SlideUp';
-export { AnimatedCard } from './AnimatedCard';
-export type { AnimatedCardProps } from './AnimatedCard';
